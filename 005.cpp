282df4c24f31d143aaa002cf203fabf2a6c2a03f
XJTU7Leo <419703933@qq.com>
2021-09-26 23:23:41 +0800

    chapter1 and chapter2

---
+++ 005.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,x,y;
+	short b;
+	x=sizeof(a);
+	y=sizeof(b);
+	cout<<x<<" "<<y<<endl;
+	return 0;
+}
---
+++ 006.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a;
+	double b;
+	int x,y;
+	x=sizeof(a);
+	y=sizeof(b);
+	cout<<x<<" "<<y<<endl;
+	return 0;
+}
